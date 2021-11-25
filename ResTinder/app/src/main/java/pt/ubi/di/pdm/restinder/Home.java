e2a69904d462d9f6b266e1287a3db96c125cea89
monteiro141 <monteiro141@hotmail.com>
2021-11-25 18:20:53 +0000

    changes home mainactivity and register

--- ResTinder/app/src/main/java/pt/ubi/di/pdm/restinder/Home.java
+++ ResTinder/app/src/main/java/pt/ubi/di/pdm/restinder/Home.java
@@ -1,6 +1,72 @@
 package pt.ubi.di.pdm.restinder;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class Home extends Activity {
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Home extends Activity
+{
+    private ImageButton logout;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        mAuth = FirebaseAuth.getInstance();
+        logout = (ImageButton) findViewById(R.id.btn_back);
+
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Home.this,MainActivity.class));
+            }
+        });
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID= user.getUid();
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            /**
+             * Vai buscar os dados no realtime database do user que inicou sessão
+             */
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User userProfile = dataSnapshot.getValue(User.class);
+                if(userProfile != null){
+                    String email = userProfile.email;
+                    String firstName = userProfile.firstName;
+                    String lastName = userProfile.lastName;
+                    String birthday = userProfile.birthday;
+                    String state = userProfile.state;
+                    String gender = userProfile.gender;
+                    String phone = userProfile.phone;
+                    String interestedIn = userProfile.interestedIn;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(Home.this,"Failed to login!",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
--- ResTinder/app/src/main/java/pt/ubi/di/pdm/restinder/MainActivity.java
+++ ResTinder/app/src/main/java/pt/ubi/di/pdm/restinder/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,16 +26,49 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mAuth = FirebaseAuth.getInstance();
 
 
 
     }
 
-    private void userLogin()
-    {
+    private void userLogin() {
+        String email = "test@email.com"; // editTextEmail.getText().toString().trim();
+        String password = "test123456";
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches() || email.isEmpty()) {
+            System.out.println("Invalid email");
+            /*
+             * editTextEmail.setError("Invalid email!");
+             * editTextEmail.requestFocus();
+             * return;
+             * */
+        }
+        if(password.isEmpty()) {
+            System.out.println("Password is empty");
+            /*
+             * editTextPassword.setError("Password is empty!");
+             * editTextPassword.requestFocus();
+             * return;
+             * */
+        }
 
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if(user.isEmailVerified()) {
+                        startActivity(new Intent(MainActivity.this, Home.class));
+                    }else{
+                        user.sendEmailVerification();
+                        Toast.makeText(MainActivity.this,"Check your email to verify your account!",Toast.LENGTH_LONG).show();
+                    }
+
+                }else {
+                    Toast.makeText(MainActivity.this,"Failed to login!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
 
